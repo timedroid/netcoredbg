4559a3e829d271d5140082bd2e5e9c91ec31b882
William Kent <wjk011@gmail.com>
2020-07-02 23:05:39 -0400

    Update to latest .NET 5 preview

--- fetchdeps.cmake
+++ fetchdeps.cmake
@@ -26,7 +26,7 @@ if ("${CORECLR_DIR}" STREQUAL "")
             file(REMOVE_RECURSE "${CORECLR_DIR}")
         endif()
         execute_process(
-            COMMAND ${GIT_EXECUTABLE} clone --progress --depth 1 https://github.com/dotnet/coreclr "${CORECLR_DIR}" -b "${CORECLR_BRANCH}"
+            COMMAND ${GIT_EXECUTABLE} clone --progress --depth 1 https://github.com/dotnet/runtime "${CORECLR_DIR}" -b "${CORECLR_BRANCH}"
             WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
             RESULT_VARIABLE retcode)
         if (NOT "${retcode}" STREQUAL "0")
@@ -37,30 +37,5 @@ endif()
 
 # Fetch .NET SDK binaries if necessary
 if ("${DOTNET_DIR}" STREQUAL "" AND (("${DBGSHIM_RUNTIME_DIR}" STREQUAL "") OR ${BUILD_MANAGED}))
-    set(DOTNET_DIR ${CMAKE_CURRENT_SOURCE_DIR}/.dotnet)
-
-    if (WIN32)
-        execute_process(
-            COMMAND powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 ; (new-object System.Net.WebClient).DownloadFile('https://dot.net/v1/dotnet-install.ps1', '${CMAKE_CURRENT_BINARY_DIR}/dotnet-install.ps1')"
-            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-            RESULT_VARIABLE retcode)
-        if (NOT "${retcode}" STREQUAL "0")
-            message(FATAL_ERROR "Fatal error when downloading dotnet install script")
-        endif()
-        execute_process(
-            COMMAND powershell -File "${CMAKE_CURRENT_BINARY_DIR}/dotnet-install.ps1" -Channel "${DOTNET_CHANNEL}" -InstallDir "${DOTNET_DIR}" -Architecture x64 -Verbose
-            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-            RESULT_VARIABLE retcode)
-        if (NOT "${retcode}" STREQUAL "0")
-            message(FATAL_ERROR "Fatal error when installing dotnet")
-        endif()
-        else()
-        execute_process(
-            COMMAND bash -c "curl -sSL \"https://dot.net/v1/dotnet-install.sh\" | bash /dev/stdin --channel \"${DOTNET_CHANNEL}\" --install-dir \"${DOTNET_DIR}\" --verbose"
-            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-            RESULT_VARIABLE retcode)
-        if (NOT "${retcode}" STREQUAL "0")
-            message(FATAL_ERROR "Fatal error when installing dotnet")
-        endif()
-    endif()
+    message(FATAL_ERROR "Please specify \${DOTNET_DIR}")
 endif()
